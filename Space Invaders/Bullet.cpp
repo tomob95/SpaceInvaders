7290717b9bbede070310ba5bbc6612ff24e3d829
Tattynana <Kelsey.scheurich@gmail.com>
2014-11-15 15:32:31 +1300

    Moar comments
    Bullet & backbuffer

--- Space Invaders/Bullet.cpp
+++ Space Invaders/Bullet.cpp
@@ -1,79 +1,177 @@
+//
+// Bachelor of Software Engineering
+// Media Design School
+// Auckland
+// New Zealand
+//
+// 2014 (c) Media Design School
+//
+// File Name	: bullet.cpp
+// Description	: Implementation file for bullet class
+// Author		: Kelsey Scheurich, Thomas O'Brien
+// Mail			: kelsey.scheurich@mediadesign.school.nz
+//
+
 // Library Includes
+#include <Windows.h>
+
 // Local Includes
 #include "resource.h"
 #include "utils.h"
-#include <Windows.h>
+#include "level.h"
+
 // This Include
 #include "bullet.h"
-#include "level.h"
+
 // Static Variables
 // Static Function Prototypes
 
 // Implementation
+/***********************
+
+ * CBackBuffer: Constructor
+ * @author: 
+ * @parameter: bool _bDirection, direction of bullet
+ *				int _iXPos, x position
+ *				int _iYPos, y position
+
+ ********************/
 CBullet::CBullet(bool _bDirection, int _iXPos, int _iYPos)
-: m_bHit(false),
-m_bDirection(_bDirection)
+	: m_bHit(false),
+	  m_bDirection(_bDirection) // Set direction as parameter
 {
-	m_fX =_iXPos;
+	// Set x & y position
+	m_fX =_iXPos; 
 	m_fY = _iYPos;
 }
+
+/***********************
+
+ * ~CBackBuffer: Destructor
+ * @author: 
+
+ ********************/
 CBullet::~CBullet()
 {
 }
 
+/***********************
+
+ * Initialise: Destructor
+ * @author:
+ * @return: bool
+
+ ********************/
 bool CBullet::Initialise()
 {
-
+	// Validate and initialise entitiy
 	VALIDATE(CEntity::Initialise(IDB_INVADER));
 
 	return (true);
 }
 
+/***********************
+
+ * Draw: Draw the bullet
+ * @author:
+
+ ********************/
 void CBullet::Draw()
 {
+	// If the bullet is not hit
 	if (!m_bHit)
 	{
+		// Draw the bullet
 		CEntity::Draw();
 	}
 }
 
+/***********************
+
+ * SetDirection: Set direction of bullet
+ * @author:
+ * @parameter: bool _bDirection, direction bullet is travelling
+
+ ********************/
 void CBullet::SetDirection(bool _bDirection)
 {
+	// Set member variable
 	m_bDirection = _bDirection;
 }
 
+/***********************
+
+ * GetDirection: Get direction of bullet
+ * @author:
+ * @return: bool
+
+ ********************/
 bool CBullet::GetDirection()
 {
-	return m_bDirection;
+	return( m_bDirection );
 }
 
+/***********************
+
+ * Process: Process bullet
+ * @author:
+ * @parameter: float _fDeltaTick, delta time
+
+ ********************/
 void CBullet::Process(float _fDeltaTick)
 {
+	// If bullet is not hit	
 	if (!m_bHit)
 	{
+		// If direction is up ( true )
 		if (m_bDirection)
 		{
+			// Change y position adding current & speed
 			m_fY = m_fY + kiBulletSpeed;
 		}
 		else
 		{
+			// Change y position subtracting current & speed
 			m_fY = m_fY - kiBulletSpeed;
 		}
+		// Process entity
 		CEntity::Process(_fDeltaTick);
 	}
 }
 
+/***********************
+
+ * ProcessSprite: Process bullet sprite
+ * @author:
+ * @parameter: float _fDeltaTick, delta time
+
+ ********************/
 void CBullet::ProcessSprite(float _fDeltaTick)
 {
+	// Call process for entity
 	CEntity::Process(_fDeltaTick);
 }
 
+/***********************
+
+ * SetHit: set bullet hit
+ * @author:
+ * @parameter: bool _b, whether bullet is hit
+
+ ********************/
 void CBullet::SetHit(bool _b)
 {
 	m_bHit = _b;
 }
 
+/***********************
+
+ * GetHit: Get bullet hit
+ * @author:
+ * @return: bool
+
+ ********************/
 bool CBullet::IsHit() const
 {
-	return (m_bHit);
+	return( m_bHit );
 }
--- Space Invaders/backbuffer.cpp
+++ Space Invaders/backbuffer.cpp
@@ -1,13 +1,29 @@
-// Library Includes
-// Local Includes
+//
+// Bachelor of Software Engineering
+// Media Design School
+// Auckland
+// New Zealand
+//
+// 2014 (c) Media Design School
+//
+// File Name	: backbuffer.cpp
+// Description	: Implementation file for backbuffer class
+// Author		: Kelsey Scheurich, Thomas O'Brien
+// Mail			: kelsey.scheurich@mediadesign.school.nz
+//
+
 // This include
 #include "BackBuffer.h"
-// Static Variables
-// Static Function Prototypes
 
 // Implementation
+/***********************
+
+ * CBackBuffer: Constructor
+ * @author: 
+
+ ********************/
 CBackBuffer::CBackBuffer()
-			: m_hWnd(0)
+			: m_hWnd(0)	// Default to 0
 			, m_hDC(0)
 			, m_hSurface(0)
 			, m_hOldObject(0)
@@ -16,58 +32,119 @@ CBackBuffer::CBackBuffer()
 {
 }
 
+/***********************
+
+ * ~CBackBuffer: Destructor
+ * @author: 
+
+ ********************/
 CBackBuffer::~CBackBuffer()
 {
+	// Delete objects
 	SelectObject(m_hDC, m_hOldObject);
 	DeleteObject(m_hSurface);
 	DeleteObject(m_hDC);
 }
 
+/***********************
+
+ * Initialise: Initialise backbuffer
+ * @author: 
+ * @parameter: HWND _hWnd, window handler
+ *				int _iWidth, width
+ *				int _iHeight, height
+ * @return: bool
+
+ ********************/
 bool CBackBuffer::Initialise(HWND _hWnd, int _iWidth, int _iHeight)
 {
+	// Set input as member variables
 	m_hWnd = _hWnd;
 	m_iWidth = _iWidth;
 	m_iHeight = _iHeight;
 
+	// Get current windowDC using hwnd and create bitmap compatible surface
 	HDC hWindowDC = ::GetDC(m_hWnd);
 	m_hDC = CreateCompatibleDC(hWindowDC);
 	m_hSurface = CreateCompatibleBitmap(hWindowDC, m_iWidth, m_iHeight);
 	ReleaseDC(m_hWnd, hWindowDC);
 
+	// Set old object as bitmap surface, create new brush and store old brush
 	m_hOldObject = static_cast<HBITMAP>(SelectObject(m_hDC, m_hSurface));
 	HBRUSH brushWhite = static_cast<HBRUSH>(GetStockObject(LTGRAY_BRUSH));
 	HBRUSH oldBrush = static_cast<HBRUSH>(SelectObject(m_hDC, brushWhite));
 
+	// Create rectangle
 	Rectangle(m_hDC, 0, 0, m_iWidth, m_iHeight);
 	SelectObject(m_hDC, oldBrush);
 	return (true);
 }
 
+/***********************
+
+ * Clear: Clear the backbuffer
+ * @author: 
+
+ ********************/
 void CBackBuffer::Clear()
 {
+	// Set hOldBrush as current brush
 	HBRUSH hOldBrush = static_cast<HBRUSH>(SelectObject(GetBFDC(), GetStockObject(BLACK_BRUSH)));
+	// Get rectangle
 	Rectangle(GetBFDC(), 0, 0, GetWidth(), GetHeight());
+	// Clear rectangle
 	SelectObject(GetBFDC(), hOldBrush);
 }
 
+/***********************
+
+ * GetBFDC: Return m_hDC
+ * @author: 
+ * @return: HDC
+
+ ********************/
 HDC CBackBuffer::GetBFDC() const
 {
 	return (m_hDC);
 }
 
+/***********************
+
+ * GetWidth: Return m_iWidth
+ * @author: 
+ * @return: int
+
+ ********************/
 int CBackBuffer::GetWidth() const
 {
 	return (m_iWidth);
 }
 
+/***********************
+
+ * GetHeight: Return m_iHeight
+ * @author: 
+ * @return: int
+
+ ********************/
 int CBackBuffer::GetHeight() const
 {
 	return (m_iHeight);
 }
 
+/***********************
+
+ * Present: Get current HDC and update present
+ * @author: 
+ * @return: int
+
+ ********************/
 void CBackBuffer::Present()
 {
+	// Get current HDC 
 	HDC hWndDC = ::GetDC(m_hWnd);
+	// Update to present
 	BitBlt(hWndDC, 0, 0, m_iWidth, m_iHeight, m_hDC, 0, 0, SRCCOPY);
+	// Release
 	ReleaseDC(m_hWnd, hWndDC);
 }
